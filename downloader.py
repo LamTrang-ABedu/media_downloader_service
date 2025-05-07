2fa2149334fc178fdc10ae2741c083fe872a33c9
Lam Trang <lamtrang@abedu.vn>
2025-05-07 11:38:17 +0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -47,7 +47,7 @@ def download_from_url(url):
 
         elif domain == 'youtube.com' or domain == 'youtu.be':
             ydl_opts.update({
-                cookiefile': cookiefile,
+                'cookiefile': cookiefile,
                 'geo_bypass': True,
                 'geo_bypass_country': 'US',
                 'quiet': False,
