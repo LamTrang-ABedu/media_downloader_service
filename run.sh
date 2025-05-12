63e9a4691615bf7a5c14d3cd2e76a60ddfc19cd9
lampv <lampv01203@gmail.com>
2025-05-13 04:01:16 +0700

    a

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 cd /opt/hopehub/media_downloader_service
 git pull
 pip3 install -r requirements.txt
-pkill -f "5003"
-nohup gunicorn -w 2 -b 0.0.0.0:5003 app:app > log.txt 2>&1 &
+exec gunicorn -w 2 -b 0.0.0.0:5003 app:app
